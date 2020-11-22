fafb2b817b0b8ead262004a31b371e4e91039197
Damian Garces <guners86@hotmail.com>
2020-11-22 12:56:58 -0500

    Update packages

--- run-scanner-sonarqube.ps1
+++ run-scanner-sonarqube.ps1
@@ -1,7 +1,16 @@
-dotnet test .\tests\CodeDesignPlus.Core.Test\CodeDesignPlus.Core.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+#Ejecutar los siguientes comandos en la ocnsola de nuget
 
-dotnet sonarscanner begin /k:"CodeDesignPlus.Core" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="tests\CodeDesignPlus.Core.Test\coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs"
+# Vars
+$path = "tests\CodeDesignPlus.Core.Test"
+$project = "$path\CodeDesignPlus.Core.Test.csproj"
+$report = "$path\coverage.opencover.xml"
+
+# Run Sonnar Scanner
+dotnet test $project /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+
+dotnet sonarscanner begin /k:"CodeDesignPlus.Core" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="$report" /d:sonar.coverage.exclusions="**Test*.cs"
 
 dotnet build
 
 dotnet sonarscanner end
+
